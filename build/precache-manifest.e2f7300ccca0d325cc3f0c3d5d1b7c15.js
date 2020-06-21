b7cb09179779ec7bf39002a6cef9add797fce7a6
veeru153 <32760644+veeru153@users.noreply.github.com>
2020-06-21 17:47:12 +0530

    Created Express App to Host React App

---
+++ build/precache-manifest.e2f7300ccca0d325cc3f0c3d5d1b7c15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd8dad2e417a1c84e4053341e07cbb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f1e6508b41d5d21dc7c",
+    "url": "/static/css/main.6b60c1ef.chunk.css"
+  },
+  {
+    "revision": "b8dfdd379b8ed3509849",
+    "url": "/static/js/2.abdc9991.chunk.js"
+  },
+  {
+    "revision": "3263eb4fa2a4ed54bb570011085a455d",
+    "url": "/static/js/2.abdc9991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f1e6508b41d5d21dc7c",
+    "url": "/static/js/main.92e5aa0f.chunk.js"
+  },
+  {
+    "revision": "61bd5f95b95eea20e795",
+    "url": "/static/js/runtime-main.2a5af8b7.js"
+  },
+  {
+    "revision": "6801b1d479c36a5f9b75d446ba6104ad",
+    "url": "/static/media/Roboto-VariableFont.6801b1d4.ttf"
+  }
+]);
--- src/components/Cards/Cards.js
+++
@@ -1,6 +0,0 @@
-import CovidBeds from './CovidBeds';
-import CovidPatients from './CovidPatients';
-import EmptyBeds from './EmptyBeds';
-import EmptyVentilators from './EmptyVentilators';
-
-export { CovidBeds, CovidPatients, EmptyBeds, EmptyVentilators };
